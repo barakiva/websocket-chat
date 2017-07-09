46a76c5abbc36fa76e555e7edd63953044e25966
Jeet Prakash <jeet1989a@gmail.com>
2017-07-09 15:16:42 +0530

    enabling CORS on server

--- server/src/main/java/com/chat/controller/AppController.java
+++ server/src/main/java/com/chat/controller/AppController.java
@@ -6,14 +6,12 @@ import java.util.Optional;
 
 import javax.servlet.http.HttpServletResponse;
 
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import com.chat.pojo.LoginRequest;
 import com.chat.pojo.User;
 
+@CrossOrigin
 @RestController
 public class AppController {
 	
